b1e759904b240f5381b07ec705639c0c7e8a6322
Deepakjot Singh <127157784+deepak5092@users.noreply.github.com>
2026-09-15 17:27:39 -0700

    Add solution: Contains Duplicate

---
+++ Easy/contains-duplicate/solution-2026-09-15_17-27-38.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        visit = set()
+
+        for n in nums:
+            if n in visit:
+                return True
+            visit.add(n)
+        
+        return False
